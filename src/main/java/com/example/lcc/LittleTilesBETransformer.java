5bb6dcbfbd08cb17e4fd0475a3826e4b2aa5c95c
Sam <saedwar@umich.edu>
2026-08-19 16:42:41 -0500

    Port LittleTiles block entity transformer

---
+++ src/main/java/com/example/lcc/LittleTilesBETransformer.java
@@ -0,0 +1,71 @@
+package com.example.lcc;
+
+import com.simibubi.create.api.contraption.transformable.MovedBlockTransformerRegistries;
+import com.simibubi.create.content.contraptions.StructureTransform;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import team.creative.littletiles.common.block.entity.BETiles;
+import team.creative.littletiles.common.math.transformation.LittleBlockTransformer;
+
+public class LittleTilesBETransformer
+        implements MovedBlockTransformerRegistries.BlockEntityTransformer {
+
+    @Override
+    public void transform(BlockEntity be, StructureTransform transform) {
+        if (!(be instanceof BETiles tilesBe)) {
+            return;
+        }
+
+        Direction.Axis axis = transform.rotationAxis;
+        Rotation mcRot = transform.rotation;
+
+        if (axis == null || mcRot == null || mcRot == Rotation.NONE) {
+            return;
+        }
+
+        int steps;
+
+        switch (mcRot) {
+            case CLOCKWISE_90:
+                steps = 3;
+                break;
+
+            case CLOCKWISE_180:
+                steps = 2;
+                break;
+
+            case COUNTERCLOCKWISE_90:
+                steps = 1;
+                break;
+
+            default:
+                return;
+        }
+
+        team.creative.creativecore.common.util.math.transformation.Rotation ltRot;
+
+        switch (axis) {
+            case Y:
+                ltRot =
+                        team.creative.creativecore.common.util.math.transformation.Rotation.Y_CLOCKWISE;
+                break;
+
+            case X:
+                ltRot =
+                        team.creative.creativecore.common.util.math.transformation.Rotation.X_CLOCKWISE;
+                break;
+
+            case Z:
+                ltRot =
+                        team.creative.creativecore.common.util.math.transformation.Rotation.Z_CLOCKWISE;
+                break;
+
+            default:
+                return;
+        }
+
+        LittleBlockTransformer.rotate(tilesBe, ltRot, steps);
+        tilesBe.updateTiles(false, true);
+    }
+}
--- src/main/java/com/example/lcc/LittletilesCreateCompat.java
+++ src/main/java/com/example/lcc/LittletilesCreateCompat.java
@@ -1,12 +1,40 @@
 package com.example.lcc;
 
+import com.simibubi.create.api.contraption.transformable.MovedBlockTransformerRegistries;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import team.creative.littletiles.LittleTilesRegistry;
 
 @Mod(LittletilesCreateCompat.MOD_ID)
 public class LittletilesCreateCompat {
 
     public static final String MOD_ID = "lcc";
 
-    public LittletilesCreateCompat() {
+    public LittletilesCreateCompat(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+
+            LittleTilesBETransformer transformer =
+                    new LittleTilesBETransformer();
+
+            MovedBlockTransformerRegistries.BLOCK_ENTITY_TRANSFORMERS.register(
+                    LittleTilesRegistry.BE_TILES_TYPE.value(),
+                    transformer
+            );
+
+            MovedBlockTransformerRegistries.BLOCK_ENTITY_TRANSFORMERS.register(
+                    LittleTilesRegistry.BE_TILES_TYPE_RENDERED.value(),
+                    transformer
+            );
+
+            MovedBlockTransformerRegistries.BLOCK_ENTITY_TRANSFORMERS.register(
+                    LittleTilesRegistry.BE_SIGNALCONVERTER_TYPE.value(),
+                    transformer
+            );
+        });
     }
 }
